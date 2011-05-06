289f3bc719dd676c066e8c426439a8948b7e4bb0
Bob Lee <crazybob@crazybob.org>
2011-05-06 14:26:56 -0700

    Moved echo up.

--- modules/android/build-jni.sh
+++ modules/android/build-jni.sh
@@ -4,8 +4,8 @@ if [ ! -d android-ndk ]
 then
   echo Downloading Android NDK...
   curl http://dl.google.com/android/ndk/android-ndk-r5b-darwin-x86.tar.bz2 > /tmp/android-ndk.tgz
-  tar zxf /tmp/android-ndk.tgz -C /tmp
   echo Extracting Android NDK...
+  tar zxf /tmp/android-ndk.tgz -C /tmp
   mv /tmp/android-ndk-r5b ./android-ndk
 fi
 
