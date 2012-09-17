f4872e8a7e74d00afded5cbd4e9780df71151180
Jake Wharton <jw@squareup.com>
2012-09-16 23:25:46 -0700

    Remove web URL and ignore SSL boolean from Server.
    These do not belong here.

--- http/src/main/java/retrofit/http/Server.java
+++ http/src/main/java/retrofit/http/Server.java
@@ -12,59 +12,27 @@ public class Server {
   public static final String DEFAULT_TYPE = "production";
 
   private final String apiUrl;
-  private final String webUrl;
   private final String type;
-  private final boolean ignoreSslWarnings;
 
-  public Server(String apiUrl, String webUrl) {
-    this(apiUrl, webUrl, false);
+  public Server(String apiUrl) {
+    this(apiUrl, DEFAULT_TYPE);
   }
 
-  public Server(String apiUrl, String webUrl, boolean ignoreSslWarnings) {
-    this(apiUrl, webUrl, DEFAULT_TYPE, ignoreSslWarnings);
-  }
-
-  public Server(String apiUrl, String webUrl, String type, boolean ignoreSslWarnings) {
+  public Server(String apiUrl, String type) {
     if (!apiUrl.endsWith("/")) {
       apiUrl += "/";
     }
     this.apiUrl = apiUrl;
-
-    if (!webUrl.endsWith("/")) {
-      webUrl += "/";
-    }
-    this.webUrl = webUrl;
-
     this.type = type;
-    this.ignoreSslWarnings = ignoreSslWarnings;
   }
 
-  /**
-   * Gets the base API url. Includes a trailing '/'.
-   */
+  /** Gets the base API url. Includes a trailing '/'. */
   public String apiUrl() {
     return apiUrl;
   }
 
-  /**
-   * Gets the base URL for Square's web site. Includes a trailing '/'.
-   */
-  public String webUrl() {
-    return webUrl;
-  }
-
-  /**
-   * Gets a human-readable server type for differentiating between multiple instances.
-   */
+  /** Gets a human-readable server type for differentiating between multiple instances. */
   public String type() {
     return type;
-  }
-
-  /**
-   * Returns true if we should ignore SSL warnings. Returns false by default.
-   * Ignored for development servers.
-   */
-  public boolean ignoreSslWarnings() {
-    return ignoreSslWarnings;
   }
 }
