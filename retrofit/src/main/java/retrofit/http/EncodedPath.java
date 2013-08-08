d99bf426f7e18b863c1da009f3ec3e4fc7eb3384
Jake Wharton <jw@squareup.com>
2013-08-07 22:48:56 -0700

    Add @Documented to the new annotations.

--- retrofit/src/main/java/retrofit/http/EncodedPath.java
+++ retrofit/src/main/java/retrofit/http/EncodedPath.java
@@ -15,6 +15,7 @@
  */
 package retrofit.http;
 
+import java.lang.annotation.Documented;
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
 
@@ -33,7 +34,9 @@ import static java.lang.annotation.RetentionPolicy.RUNTIME;
  * <p>
  * Path parameters may not be {@code null}.
  */
-@Retention(RUNTIME) @Target(PARAMETER)
+@Documented
+@Retention(RUNTIME)
+@Target(PARAMETER)
 public @interface EncodedPath {
   String value();
 }
