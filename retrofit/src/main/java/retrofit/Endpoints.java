27b2070c6b675c961d811e4805fadc9cf9227f32
Matt Hickman <mhickman@palantir.com>
2014-01-23 15:16:33 -0800

    Endpoints final + private no-arg constructor.

--- retrofit/src/main/java/retrofit/Endpoints.java
+++ retrofit/src/main/java/retrofit/Endpoints.java
@@ -1,12 +1,14 @@
 package retrofit;
 
 /**
- * Static factory methods for creating {@link Endpoint}
- * instances.
+ * Static factory methods for creating {@link Endpoint} instances.
  *
  * @author Matt Hickman (mhickman@palantir.com)
  */
-public abstract class Endpoints {
+public final class Endpoints {
+
+  private Endpoints() {
+  }
 
   /** Create a server with the provided URL. */
   public static Endpoint newFixedEndpoint(String url) {
