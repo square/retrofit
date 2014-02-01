aab048a7a8a113988bdee397599484de335a15b8
Jake Wharton <jw@squareup.com>
2014-01-31 23:07:37 -0800

    Ensure Defaults conforms as a utility class.

--- retrofit/src/main/java/retrofit/client/Defaults.java
+++ retrofit/src/main/java/retrofit/client/Defaults.java
@@ -1,6 +1,10 @@
 package retrofit.client;
 
-class Defaults {
+final class Defaults {
   static final int CONNECT_TIMEOUT_MILLIS = 15 * 1000; // 15s
   static final int READ_TIMEOUT_MILLIS = 20 * 1000; // 20s
+
+  private Defaults() {
+    // No instances.
+  }
 }
