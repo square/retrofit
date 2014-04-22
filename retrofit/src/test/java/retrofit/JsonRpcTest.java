bc5f00bb5237eaeefd4e38e2052508019799a712
Dmitry Polishuk <dp@neosphere.com>
2014-04-22 23:29:33 +0400

    removed unnecessary converter

--- retrofit/src/test/java/retrofit/JsonRpcTest.java
+++ retrofit/src/test/java/retrofit/JsonRpcTest.java
@@ -10,16 +10,10 @@ import com.google.gson.GsonBuilder;
 
 import org.junit.Before;
 import org.junit.Test;
-import org.mockito.ArgumentCaptor;
-import org.mockito.invocation.InvocationOnMock;
-import org.mockito.stubbing.Answer;
 
 import java.io.ByteArrayInputStream;
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.concurrent.Executor;
@@ -28,40 +22,19 @@ import retrofit.client.Client;
 import retrofit.client.Header;
 import retrofit.client.Request;
 import retrofit.client.Response;
-import retrofit.converter.ConversionException;
-import retrofit.http.Body;
-import retrofit.http.FormUrlEncoded;
-import retrofit.http.GET;
-import retrofit.http.Headers;
 import retrofit.http.JsonRpcMethod;
 import retrofit.http.POST;
-import retrofit.http.Path;
 import retrofit.http.RpcParam;
 import retrofit.mime.TypedInput;
-import retrofit.mime.TypedOutput;
-import retrofit.mime.TypedString;
-import retrofit.rpc.JsonRpcConverter;
-import rx.Observable;
-import rx.functions.Action1;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.fail;
 import static org.mockito.Matchers.any;
 import static org.mockito.Matchers.anyInt;
-import static org.mockito.Matchers.anyString;
 import static org.mockito.Matchers.eq;
-import static org.mockito.Matchers.isA;
 import static org.mockito.Matchers.same;
-import static org.mockito.Mockito.atLeastOnce;
-import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.verifyZeroInteractions;
 import static org.mockito.Mockito.when;
-import static retrofit.RestAdapter.LogLevel.BASIC;
-import static retrofit.RestAdapter.LogLevel.FULL;
-import static retrofit.RestAdapter.LogLevel.HEADERS;
 
 public class JsonRpcTest {
   private static final List<Header> NO_HEADERS = Collections.emptyList();
@@ -113,7 +86,6 @@ public class JsonRpcTest {
         .setExecutors(mockRequestExecutor, mockCallbackExecutor)
         .setEndpoint("http://example.com:3344/api/")
         .setProfiler(mockProfiler)
-        .setConverter(new JsonRpcConverter(gson))
         .setLogLevel(RestAdapter.LogLevel.FULL)
         .build()
         .create(Example.class);
