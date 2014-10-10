468e0efe878ff66f156114cf2fcbb5fc7d79a513
Jake Wharton <jw@squareup.com>
2014-10-09 23:56:55 -0700

    Remove endpoint names.
    This isn't the right granularity of data about an endpoint and we never exposed this anywhere in the actual library.

--- retrofit/src/main/java/retrofit/Endpoints.java
+++ retrofit/src/main/java/retrofit/Endpoints.java
@@ -6,36 +6,15 @@ package retrofit;
  * @author Matt Hickman (mhickman@palantir.com)
  */
 public final class Endpoints {
-  private static final String DEFAULT_NAME = "default";
-
   private Endpoints() {
   }
 
   /** Create a server with the provided URL. */
-  public static Endpoint newFixedEndpoint(String url) {
-    return new FixedEndpoint(url, DEFAULT_NAME);
-  }
-
-  /** Create an endpoint with the provided URL and name. */
-  public static Endpoint newFixedEndpoint(String url, String name) {
-    return new FixedEndpoint(url, name);
-  }
-
-  private static class FixedEndpoint implements Endpoint {
-    private final String apiUrl;
-    private final String name;
-
-    FixedEndpoint(String apiUrl, String name) {
-      this.apiUrl = apiUrl;
-      this.name = name;
-    }
-
-    @Override public String getUrl() {
-      return apiUrl;
-    }
-
-    @Override public String getName() {
-      return name;
-    }
+  public static Endpoint newFixedEndpoint(final String url) {
+    return new Endpoint() {
+      @Override public String getUrl() {
+        return url;
+      }
+    };
   }
 }
