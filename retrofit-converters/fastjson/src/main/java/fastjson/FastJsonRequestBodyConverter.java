edfefd5df3809f29c8eec6d785b858523cef2809
xiongfeng <xiongfeng@ifeng.com>
2016-03-02 15:39:53 +0800

    add converter-fastjson module

---
+++ retrofit-converters/fastjson/src/main/java/fastjson/FastJsonRequestBodyConverter.java
@@ -0,0 +1,27 @@
+package fastjson;
+
+import com.alibaba.fastjson.JSONWriter;
+import okhttp3.MediaType;
+import okhttp3.RequestBody;
+import okio.Buffer;
+import retrofit2.Converter;
+
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.nio.charset.Charset;
+
+
+public final class FastJsonRequestBodyConverter<T> implements Converter<T, RequestBody> {
+    private static final MediaType MEDIA_TYPE = MediaType.parse("application/json; charset=UTF-8");
+    private static final Charset UTF8 = Charset.forName("UTF-8");
+
+    @Override
+    public RequestBody convert(T value) throws IOException {
+        Buffer buffer = new Buffer();
+        OutputStreamWriter outputStreamWriter = new OutputStreamWriter(buffer.outputStream(), UTF8);
+        JSONWriter jsonWriter = new JSONWriter(outputStreamWriter);
+        jsonWriter.writeObject(value);
+        jsonWriter.close();
+        return RequestBody.create(MEDIA_TYPE, buffer.readByteString());
+    }
+}
---
+++ retrofit-converters/fastjson/src/main/java/fastjson/FastJsonResponseBodyConverter.java
@@ -0,0 +1,23 @@
+package fastjson;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.parser.Feature;
+import okhttp3.ResponseBody;
+import retrofit2.Converter;
+
+import java.io.IOException;
+import java.lang.reflect.Type;
+
+public final class FastJsonResponseBodyConverter<T> implements Converter<ResponseBody, T> {
+
+    private Type type;
+
+    public FastJsonResponseBodyConverter(Type type) {
+        this.type = type;
+    }
+
+    @Override
+    public T convert(ResponseBody value) throws IOException {
+        return JSON.parseObject(value.string(), type, Feature.AllowComment);
+    }
+}
