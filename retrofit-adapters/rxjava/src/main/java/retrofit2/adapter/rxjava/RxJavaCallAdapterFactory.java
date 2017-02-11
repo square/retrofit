823f9692cc8929bcb273e63f880c6a872e3b55ce
John Carlson <jawnnypoo@gmail.com>
2017-02-11 01:36:38 -0600

    Fix code style

--- retrofit-adapters/rxjava/src/main/java/retrofit2/adapter/rxjava/RxJavaCallAdapterFactory.java
+++ retrofit-adapters/rxjava/src/main/java/retrofit2/adapter/rxjava/RxJavaCallAdapterFactory.java
@@ -20,6 +20,7 @@ import java.lang.annotation.Annotation;
 import java.lang.reflect.ParameterizedType;
 import java.lang.reflect.Type;
 import retrofit2.CallAdapter;
+import retrofit2.HttpException;
 import retrofit2.Response;
 import retrofit2.Retrofit;
 import rx.Completable;
@@ -42,7 +43,7 @@ import rx.Single;
  * parameter:
  * <ul>
  * <li>Direct body (e.g., {@code Observable<User>}) calls {@code onNext} with the deserialized body
- * for 2XX responses and calls {@code onError} with {@link retrofit2.HttpException} for non-2XX responses and
+ * for 2XX responses and calls {@code onError} with {@link HttpException} for non-2XX responses and
  * {@link IOException} for network errors.</li>
  * <li>Response wrapped body (e.g., {@code Observable<Response<User>>}) calls {@code onNext}
  * with a {@link Response} object for all HTTP responses and calls {@code onError} with
