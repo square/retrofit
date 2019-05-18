7feb317126d29e1d07f9cb641439aa2b9c0aad5e
Nitrogen <13231528+nitrohsu@users.noreply.github.com>
2019-05-18 16:46:38 +0800

    Remove serviceMethodCache synchronized lock
    serviceMethodCache now is Thread-safe's ConcurrentHashMap, so the performance is better if delete the sync lock

--- retrofit/src/main/java/retrofit2/Retrofit.java
+++ retrofit/src/main/java/retrofit2/Retrofit.java
@@ -163,12 +163,10 @@ public final class Retrofit {
     ServiceMethod<?> result = serviceMethodCache.get(method);
     if (result != null) return result;
 
-    synchronized (serviceMethodCache) {
-      result = serviceMethodCache.get(method);
-      if (result == null) {
-        result = ServiceMethod.parseAnnotations(this, method);
-        serviceMethodCache.put(method, result);
-      }
+    result = serviceMethodCache.get(method);
+    if (result == null) {
+      result = ServiceMethod.parseAnnotations(this, method);
+      serviceMethodCache.put(method, result);
     }
     return result;
   }
