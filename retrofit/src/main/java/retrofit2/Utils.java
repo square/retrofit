948a0db7ef1d46c66237ed2feb9f6f08ee159c68
Jake Wharton <jakew@google.com>
2019-07-03 23:25:20 -0400

    Allow service interfaces to extend other interfaces
    With a higher minSdkVersion, the bug which prevented this on Android no longer exists.

--- retrofit/src/main/java/retrofit2/Utils.java
+++ retrofit/src/main/java/retrofit2/Utils.java
@@ -317,18 +317,6 @@ final class Utils {
     return ResponseBody.create(body.contentType(), body.contentLength(), buffer);
   }
 
-  static <T> void validateServiceInterface(Class<T> service) {
-    if (!service.isInterface()) {
-      throw new IllegalArgumentException("API declarations must be interfaces.");
-    }
-    // Prevent API interfaces from extending other interfaces. This not only avoids a bug in
-    // Android (http://b.android.com/58753) but it forces composition of API declarations which is
-    // the recommended pattern.
-    if (service.getInterfaces().length > 0) {
-      throw new IllegalArgumentException("API interfaces must not extend other interfaces.");
-    }
-  }
-
   static Type getParameterUpperBound(int index, ParameterizedType type) {
     Type[] types = type.getActualTypeArguments();
     if (index < 0 || index >= types.length) {
