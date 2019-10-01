9bf56e68eb70d4118cb87c041a7c398e48041314
Zac Sweers <zac.sweers@gmail.com>
2019-10-01 03:46:46 -0400

    Stream Gson request body

--- retrofit-converters/gson/src/main/java/retrofit2/converter/gson/GsonRequestBodyConverter.java
+++ retrofit-converters/gson/src/main/java/retrofit2/converter/gson/GsonRequestBodyConverter.java
@@ -20,16 +20,16 @@ import com.google.gson.TypeAdapter;
 import com.google.gson.stream.JsonWriter;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
-import java.io.Writer;
 import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 import okhttp3.MediaType;
 import okhttp3.RequestBody;
-import okio.Buffer;
+import okio.BufferedSink;
 import retrofit2.Converter;
 
 final class GsonRequestBodyConverter<T> implements Converter<T, RequestBody> {
   private static final MediaType MEDIA_TYPE = MediaType.get("application/json; charset=UTF-8");
-  private static final Charset UTF_8 = Charset.forName("UTF-8");
+  private static final Charset UTF_8 = StandardCharsets.UTF_8;
 
   private final Gson gson;
   private final TypeAdapter<T> adapter;
@@ -39,13 +39,18 @@ final class GsonRequestBodyConverter<T> implements Converter<T, RequestBody> {
     this.adapter = adapter;
   }
 
-  @Override
-  public RequestBody convert(T value) throws IOException {
-    Buffer buffer = new Buffer();
-    Writer writer = new OutputStreamWriter(buffer.outputStream(), UTF_8);
-    JsonWriter jsonWriter = gson.newJsonWriter(writer);
-    adapter.write(jsonWriter, value);
-    jsonWriter.close();
-    return RequestBody.create(MEDIA_TYPE, buffer.readByteString());
+  @Override public RequestBody convert(T value) {
+    return new RequestBody() {
+      @Override public MediaType contentType() {
+        return MEDIA_TYPE;
+      }
+
+      @Override public void writeTo(BufferedSink bufferedSink) throws IOException {
+        try (JsonWriter writer = gson.newJsonWriter(
+            new OutputStreamWriter(bufferedSink.outputStream(), UTF_8))) {
+          adapter.write(writer, value);
+        }
+      }
+    };
   }
 }
