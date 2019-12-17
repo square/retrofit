1ec98c2364b497278c34f04a49236cde9de7b178
Jake Wharton <jakew@google.com>
2019-12-16 20:35:21 -0500

    Add coroutine support to BehaviorDelegate

--- retrofit-mock/src/main/java/retrofit2/mock/BehaviorDelegate.java
+++ retrofit-mock/src/main/java/retrofit2/mock/BehaviorDelegate.java
@@ -16,12 +16,18 @@
 package retrofit2.mock;
 
 import java.lang.annotation.Annotation;
+import java.lang.reflect.Method;
+import java.lang.reflect.ParameterizedType;
 import java.lang.reflect.Proxy;
 import java.lang.reflect.Type;
+import java.lang.reflect.WildcardType;
 import java.util.concurrent.ExecutorService;
 import javax.annotation.Nullable;
+import kotlin.coroutines.Continuation;
 import retrofit2.Call;
 import retrofit2.CallAdapter;
+import retrofit2.KotlinExtensions;
+import retrofit2.Response;
 import retrofit2.Retrofit;
 
 /**
@@ -54,11 +60,101 @@ public final class BehaviorDelegate<T> {
     final Call<R> behaviorCall = new BehaviorCall<>(behavior, executor, call);
     return (T) Proxy.newProxyInstance(service.getClassLoader(), new Class[] { service },
         (proxy, method, args) -> {
-          Type returnType = method.getGenericReturnType();
+          ServiceMethodAdapterInfo adapterInfo = parseServiceMethodAdapterInfo(method);
+
           Annotation[] methodAnnotations = method.getAnnotations();
           CallAdapter<R, T> callAdapter =
-              (CallAdapter<R, T>) retrofit.callAdapter(returnType, methodAnnotations);
-          return callAdapter.adapt(behaviorCall);
+              (CallAdapter<R, T>) retrofit.callAdapter(adapterInfo.responseType, methodAnnotations);
+
+          T adapted = callAdapter.adapt(behaviorCall);
+          if (!adapterInfo.isSuspend) {
+            return adapted;
+          }
+
+          Call<Object> adaptedCall = (Call<Object>) adapted;
+          Continuation<Object> continuation = (Continuation<Object>) args[args.length - 1];
+          try {
+            return adapterInfo.wantsResponse
+                ? KotlinExtensions.awaitResponse(adaptedCall, continuation)
+                : KotlinExtensions.await(adaptedCall, continuation);
+          } catch (Exception e) {
+            return KotlinExtensions.suspendAndThrow(e, continuation);
+          }
         });
+  }
+
+  /**
+   * Computes the adapter type of the method for lookup via {@link Retrofit#callAdapter} as well as
+   * information on whether the method is a {@code suspend fun}.
+   * <p>
+   * In the case of a Kotlin {@code suspend fun}, the last parameter type is a {@code Continuation}
+   * whose parameter carries the actual response type. In this case, we return {@code Call<T>} where
+   * {@code T} is the body type.
+   */
+  private static ServiceMethodAdapterInfo parseServiceMethodAdapterInfo(Method method) {
+    Type[] genericParameterTypes = method.getGenericParameterTypes();
+    if (genericParameterTypes.length != 0) {
+      Type lastParameterType = genericParameterTypes[genericParameterTypes.length - 1];
+      if (lastParameterType instanceof ParameterizedType) {
+        ParameterizedType parameterizedLastParameterType = (ParameterizedType) lastParameterType;
+        try {
+          if (parameterizedLastParameterType.getRawType() == Continuation.class) {
+            Type resultType = parameterizedLastParameterType.getActualTypeArguments()[0];
+            if (resultType instanceof WildcardType) {
+              resultType = ((WildcardType) resultType).getLowerBounds()[0];
+            }
+            if (resultType instanceof ParameterizedType) {
+              ParameterizedType parameterizedResultType = (ParameterizedType) resultType;
+              if (parameterizedResultType.getRawType() == Response.class) {
+                Type bodyType = parameterizedResultType.getActualTypeArguments()[0];
+                Type callType = new CallParameterizedTypeImpl(bodyType);
+                return new ServiceMethodAdapterInfo(true, true, callType);
+              }
+            }
+            Type callType = new CallParameterizedTypeImpl(resultType);
+            return new ServiceMethodAdapterInfo(true, false, callType);
+          }
+        } catch (NoClassDefFoundError ignored) {
+          // Not using coroutines.
+        }
+      }
+    }
+    return new ServiceMethodAdapterInfo(false, false, method.getGenericReturnType());
+  }
+
+  static final class CallParameterizedTypeImpl implements ParameterizedType {
+    private final Type bodyType;
+
+    CallParameterizedTypeImpl(Type bodyType) {
+      this.bodyType = bodyType;
+    }
+
+    @Override public Type[] getActualTypeArguments() {
+      return new Type[] { bodyType };
+    }
+
+    @Override public Type getRawType() {
+      return Call.class;
+    }
+
+    @Override public @Nullable Type getOwnerType() {
+      return null;
+    }
+  }
+
+  static class ServiceMethodAdapterInfo {
+    final boolean isSuspend;
+    /**
+     * Whether the suspend function return type was {@code Response<T>}.
+     * Only meaningful if {@link #isSuspend} is true.
+     */
+    final boolean wantsResponse;
+    final Type responseType;
+
+    ServiceMethodAdapterInfo(boolean isSuspend, boolean wantsResponse, Type responseType) {
+      this.isSuspend = isSuspend;
+      this.wantsResponse = wantsResponse;
+      this.responseType = responseType;
+    }
   }
 }
