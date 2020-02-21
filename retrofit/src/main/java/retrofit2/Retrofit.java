7dd19209025b42717de80427f75e532414a63027
Thomas May <tmay@clearwateranalytics.com>
2020-02-20 19:28:45 -0700

    Use computeIfAbsent method to simplify this method

--- retrofit/src/main/java/retrofit2/Retrofit.java
+++ retrofit/src/main/java/retrofit2/Retrofit.java
@@ -183,17 +183,7 @@ public final class Retrofit {
   }
 
   ServiceMethod<?> loadServiceMethod(Method method) {
-    ServiceMethod<?> result = serviceMethodCache.get(method);
-    if (result != null) return result;
-
-    synchronized (serviceMethodCache) {
-      result = serviceMethodCache.get(method);
-      if (result == null) {
-        result = ServiceMethod.parseAnnotations(this, method);
-        serviceMethodCache.put(method, result);
-      }
-    }
-    return result;
+    return serviceMethodCache.computeIfAbsent(method, (m)-> ServiceMethod.parseAnnotations(this, m));
   }
 
   /**
