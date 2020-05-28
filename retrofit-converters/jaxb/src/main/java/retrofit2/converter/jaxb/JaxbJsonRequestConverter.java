16cd8c6a98c7dcf2286009acae35b68aab12365a
MaximumQuiet <maximumquiet@gmail.com>
2020-05-28 17:27:12 +0400

    Add JAXB request/response converter implementations for JSON

---
+++ retrofit-converters/jaxb/src/main/java/retrofit2/converter/jaxb/JaxbJsonRequestConverter.java
@@ -0,0 +1,45 @@
+package retrofit2.converter.jaxb;
+
+import okhttp3.RequestBody;
+import okio.Buffer;
+import retrofit2.Converter;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+import java.io.*;
+import java.util.Map;
+
+final class JaxbJsonRequestConverter<T> implements Converter<T, RequestBody> {
+    final JAXBContext context;
+    final Map<String, Object> marshalProps;
+    final Class<T> type;
+
+    protected JaxbJsonRequestConverter(JAXBContext context, Map<String, Object> marshalProps, Class<T> type) {
+        this.context = context;
+        this.marshalProps = marshalProps;
+        this.type = type;
+    }
+
+    @Override
+    public RequestBody convert(final T value) throws IOException {
+        Buffer buffer = new Buffer();
+        try (OutputStreamWriter osw = new OutputStreamWriter(buffer.outputStream(), JaxbConverterFactory.JSON.charset().name())){
+            Marshaller marshaller = createMarshaller();
+            marshaller.marshal(value, osw);
+        } catch (JAXBException e) {
+            throw new RuntimeException(e);
+        }
+        return RequestBody.create(JaxbConverterFactory.JSON, buffer.readByteString());
+    }
+
+    private Marshaller createMarshaller() throws JAXBException {
+        Marshaller marshaller = context.createMarshaller();
+        if (!marshalProps.isEmpty()) {
+            for (Map.Entry<String, Object> entry : marshalProps.entrySet()) {
+                marshaller.setProperty(entry.getKey(), entry.getValue());
+            }
+        }
+        return marshaller;
+    }
+}
---
+++ retrofit-converters/jaxb/src/main/java/retrofit2/converter/jaxb/JaxbJsonResponseConverter.java
@@ -0,0 +1,47 @@
+package retrofit2.converter.jaxb;
+
+import okhttp3.ResponseBody;
+import retrofit2.Converter;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Unmarshaller;
+import javax.xml.transform.Source;
+import javax.xml.transform.stream.StreamSource;
+import java.io.IOException;
+import java.util.Map;
+
+final class JaxbJsonResponseConverter<T> implements Converter<ResponseBody, T> {
+    final JAXBContext context;
+    final Map<String, Object> unmarshalProps;
+    final Class<T> type;
+
+    protected JaxbJsonResponseConverter(JAXBContext context, Map<String, Object> unmarshalProps, Class<T> type) {
+        this.context = context;
+        this.unmarshalProps = unmarshalProps;
+        this.type = type;
+    }
+
+    @Override
+    public T convert(ResponseBody value) throws IOException {
+        try {
+            Source source = new StreamSource(value.charStream());
+            Unmarshaller unmarshaller = createUnmarshaller();
+            return unmarshaller.unmarshal(source, type).getValue();
+        } catch (JAXBException e) {
+            throw new RuntimeException(e);
+        } finally {
+            value.close();
+        }
+    }
+
+    private Unmarshaller createUnmarshaller() throws JAXBException {
+        Unmarshaller unmarshaller = context.createUnmarshaller();
+        if (!unmarshalProps.isEmpty()) {
+            for (Map.Entry<String, Object> entry : unmarshalProps.entrySet()) {
+                unmarshaller.setProperty(entry.getKey(), entry.getValue());
+            }
+        }
+        return unmarshaller;
+    }
+}
