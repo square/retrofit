a184424663e37b85ea5974e5670a8e9ff33f93da
Jintin <jintin.lin1018@gmail.com>
2020-12-09 00:39:44 +0800

    add missing logging

--- retrofit/src/main/java/retrofit2/OkHttpCall.java
+++ retrofit/src/main/java/retrofit2/OkHttpCall.java
@@ -247,6 +247,10 @@ final class OkHttpCall<T> implements Call<T> {
     ExceptionCatchingResponseBody catchingBody = new ExceptionCatchingResponseBody(rawBody);
     try {
       T body = responseConverter.convert(catchingBody);
+      if (body != null) {
+        logger.log(body);
+      }
+
       return Response.success(body, rawResponse);
     } catch (RuntimeException e) {
       // If the underlying source threw an exception, propagate that rather than indicating it was
