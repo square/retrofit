01462e536b5fa728b9a6342d22bac06ac7b62f1f
Goooler <wangzongler@gmail.com>
2021-09-21 23:42:12 +0800

    Bump moshi to 1.12.0

--- retrofit-converters/moshi/src/test/java/retrofit2/converter/moshi/MoshiConverterFactoryTest.java
+++ retrofit-converters/moshi/src/test/java/retrofit2/converter/moshi/MoshiConverterFactoryTest.java
@@ -260,7 +260,7 @@ public final class MoshiConverterFactoryTest {
   }
 
   @Test
-  public void failOnUnknown() throws IOException, InterruptedException {
+  public void failOnUnknown() throws IOException {
     server.enqueue(new MockResponse().setBody("{\"taco\":\"delicious\"}"));
 
     Call<AnImplementation> call = serviceFailOnUnknown.anImplementation(new AnImplementation(null));
@@ -268,7 +268,7 @@ public final class MoshiConverterFactoryTest {
       call.execute();
       fail();
     } catch (JsonDataException e) {
-      assertThat(e).hasMessage("Cannot skip unexpected NAME at $.");
+      assertThat(e).hasMessage("Cannot skip unexpected NAME at $.taco");
     }
   }
 
