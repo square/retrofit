d4d0e07ccdea672fdf0870126dcfdd1cb3eb29dd
Goooler <wangzongler@gmail.com>
2021-12-17 15:59:23 +0800

    Convert settings.gradle to kts

---
+++ settings.gradle.kts
@@ -0,0 +1,29 @@
+include(
+  ":retrofit",
+  ":retrofit:android-test",
+  ":retrofit:robovm-test",
+  ":retrofit:test-helpers",
+
+  ":retrofit-mock",
+
+  ":retrofit-adapters:guava",
+  ":retrofit-adapters:java8",
+  ":retrofit-adapters:rxjava",
+  ":retrofit-adapters:rxjava2",
+  ":retrofit-adapters:rxjava3",
+  ":retrofit-adapters:scala",
+
+  ":retrofit-converters:gson",
+  ":retrofit-converters:guava",
+  ":retrofit-converters:jackson",
+  ":retrofit-converters:java8",
+  ":retrofit-converters:jaxb",
+  ":retrofit-converters:jaxb3",
+  ":retrofit-converters:moshi",
+  ":retrofit-converters:protobuf",
+  ":retrofit-converters:scalars",
+  ":retrofit-converters:simplexml",
+  ":retrofit-converters:wire",
+
+  ":samples",
+)
