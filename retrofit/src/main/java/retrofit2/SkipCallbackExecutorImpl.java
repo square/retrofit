80a0c584a2610f829f771f68aefd8304191f73d3
brendalmalves <brenda.alves@ccc.ufcg.edu.br>
2022-07-25 21:01:09 -0300

    refact SkipCallBackExecutorImpl class

--- retrofit/src/main/java/retrofit2/SkipCallbackExecutorImpl.java
+++ retrofit/src/main/java/retrofit2/SkipCallbackExecutorImpl.java
@@ -17,39 +17,37 @@ package retrofit2;
 
 import java.lang.annotation.Annotation;
 
-// This class conforms to the annotation requirements documented on Annotation.
 final class SkipCallbackExecutorImpl implements SkipCallbackExecutor {
-  private static final SkipCallbackExecutor INSTANCE = new SkipCallbackExecutorImpl();
+    private static final SkipCallbackExecutor INSTANCE = new SkipCallbackExecutorImpl();
 
-  static Annotation[] ensurePresent(Annotation[] annotations) {
-    if (Utils.isAnnotationPresent(annotations, SkipCallbackExecutor.class)) {
-      return annotations;
+    static Annotation[] ensurePresent(Annotation[] annotations) {
+        if (Utils.isAnnotationPresent(annotations, SkipCallbackExecutor.class)) {
+            return annotations;
+        }
+
+        Annotation[] newAnnotations = new Annotation[annotations.length + 1];
+        newAnnotations[0] = SkipCallbackExecutorImpl.INSTANCE;
+        System.arraycopy(annotations, 0, newAnnotations, 1, annotations.length);
+        return newAnnotations;
     }
 
-    Annotation[] newAnnotations = new Annotation[annotations.length + 1];
-    // Place the skip annotation first since we're guaranteed to check for it in the call adapter.
-    newAnnotations[0] = SkipCallbackExecutorImpl.INSTANCE;
-    System.arraycopy(annotations, 0, newAnnotations, 1, annotations.length);
-    return newAnnotations;
-  }
+    @Override
+    public Class<? extends Annotation> annotationType() {
+        return SkipCallbackExecutor.class;
+    }
 
-  @Override
-  public Class<? extends Annotation> annotationType() {
-    return SkipCallbackExecutor.class;
-  }
+    @Override
+    public boolean equals(Object obj) {
+        return obj instanceof SkipCallbackExecutor;
+    }
 
-  @Override
-  public boolean equals(Object obj) {
-    return obj instanceof SkipCallbackExecutor;
-  }
+    @Override
+    public int hashCode() {
+        return 0;
+    }
 
-  @Override
-  public int hashCode() {
-    return 0;
-  }
-
-  @Override
-  public String toString() {
-    return "@" + SkipCallbackExecutor.class.getName() + "()";
-  }
+    @Override
+    public String toString() {
+        return String.format("@%s()", SkipCallbackExecutor.class.getName());
+    }
 }
