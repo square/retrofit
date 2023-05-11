a63410800b2582875210ab60446645af63fc380c
Knight-ZXW <nimdanoob@163.com>
2023-05-11 23:09:43 +0800

    synchronized on serviceClass to avoid lock competition when loading methods of different serviceClass

--- retrofit/src/main/java/retrofit2/Retrofit.java
+++ retrofit/src/main/java/retrofit2/Retrofit.java
@@ -163,7 +163,7 @@ public final class Retrofit {
                 Platform platform = Platform.get();
                 return platform.isDefaultMethod(method)
                     ? platform.invokeDefaultMethod(method, service, proxy, args)
-                    : loadServiceMethod(method).invoke(args);
+                    : loadServiceMethod(service,method).invoke(args);
               }
             });
   }
@@ -192,17 +192,17 @@ public final class Retrofit {
       Platform platform = Platform.get();
       for (Method method : service.getDeclaredMethods()) {
         if (!platform.isDefaultMethod(method) && !Modifier.isStatic(method.getModifiers())) {
-          loadServiceMethod(method);
+          loadServiceMethod(service, method);
         }
       }
     }
   }
 
-  ServiceMethod<?> loadServiceMethod(Method method) {
+  ServiceMethod<?> loadServiceMethod(Class<?> service, Method method) {
     ServiceMethod<?> result = serviceMethodCache.get(method);
     if (result != null) return result;
 
-    synchronized (serviceMethodCache) {
+    synchronized (service) {
       result = serviceMethodCache.get(method);
       if (result == null) {
         result = ServiceMethod.parseAnnotations(this, method);
