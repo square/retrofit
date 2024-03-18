97b06327b8704592879086258a89e713fbf7afe9
Jake Wharton <jw@squareup.com>
2024-03-18 10:34:22 -0400

    Re-enable Animal Sniffer on all JVMs

--- retrofit/src/main/java/retrofit2/Reflection.java
+++ retrofit/src/main/java/retrofit2/Reflection.java
@@ -20,6 +20,7 @@ import android.os.Build;
 import java.lang.reflect.Method;
 import java.lang.reflect.Parameter;
 import javax.annotation.Nullable;
+import org.codehaus.mojo.animal_sniffer.IgnoreJRERequirement;
 
 class Reflection {
   boolean isDefaultMethod(Method method) {
@@ -37,6 +38,7 @@ class Reflection {
     return "parameter #" + (index + 1);
   }
 
+  @IgnoreJRERequirement // Only used on JVM.
   static class Java8 extends Reflection {
     @Override
     boolean isDefaultMethod(Method method) {
@@ -67,6 +69,7 @@ class Reflection {
    * through the proxy, was not added until API 26.
    */
   @TargetApi(24)
+  @IgnoreJRERequirement // Only used on Android API 24+.
   static final class Android24 extends Reflection {
     @Override
     boolean isDefaultMethod(Method method) {
