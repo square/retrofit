6443a3bf9d1819c6ea78ab4585432b4aee90f5be
Maksim Kovalev <m.kovalev@dodopizza.com>
2024-06-10 21:21:26 +0200

    Migrate to kotlinx-serialization-json-okio

--- retrofit-converters/kotlinx-serialization-json/src/main/java/retrofit2/converter/kotlinx/serialization/json/SerializerFromJson.kt
+++ retrofit-converters/kotlinx-serialization-json/src/main/java/retrofit2/converter/kotlinx/serialization/json/SerializerFromJson.kt
@@ -4,7 +4,7 @@ import kotlinx.serialization.DeserializationStrategy
 import kotlinx.serialization.ExperimentalSerializationApi
 import kotlinx.serialization.SerializationStrategy
 import kotlinx.serialization.json.Json
-import kotlinx.serialization.json.decodeFromStream
+import kotlinx.serialization.json.okio.decodeFromBufferedSource
 import okhttp3.MediaType
 import okhttp3.RequestBody
 import okhttp3.ResponseBody
@@ -13,8 +13,8 @@ import retrofit2.converter.kotlinx.serialization.Serializer
 @ExperimentalSerializationApi
 class SerializerFromJson(override val format: Json) : Serializer() {
   override fun <T> fromResponseBody(loader: DeserializationStrategy<T>, body: ResponseBody): T {
-    val stream = body.byteStream()
-    return format.decodeFromStream(loader, stream)
+    val source = body.source()
+    return format.decodeFromBufferedSource(loader, source)
   }
 
   override fun <T> toRequestBody(contentType: MediaType, saver: SerializationStrategy<T>, value: T): RequestBody {
