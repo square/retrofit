a27053babe1cd5f4877aae580ddd04c690b469aa
AmirMohhammadYZ <amir.yazdanmanesh@gmail.com>
2024-08-12 12:34:20 +0330

    Fix improve Field.java code documents

--- retrofit/src/main/java/retrofit2/http/Field.java
+++ retrofit/src/main/java/retrofit2/http/Field.java
@@ -1,18 +1,3 @@
-/*
- * Copyright (C) 2013 Square, Inc.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package retrofit2.http;
 
 import static java.lang.annotation.ElementType.PARAMETER;
@@ -66,6 +51,10 @@ import retrofit2.Retrofit;
 public @interface Field {
   String value();
 
-  /** Specifies whether the {@linkplain #value() name} and value are already URL encoded. */
+  /**
+   * Specifies whether the {@linkplain #value() name} and value are already URL encoded.
+   *
+   * @return True if the field is already encoded, false otherwise.
+   */
   boolean encoded() default false;
 }
